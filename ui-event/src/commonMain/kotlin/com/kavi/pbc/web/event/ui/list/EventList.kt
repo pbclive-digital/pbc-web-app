ce853810464dc6924da25bc06e74f98b0628b894
Kavimal Wijewardana <kavimalw@gmail.com>
2026-04-05 11:10:53 -0400

    Change event list UI for web

--- ui-event/src/commonMain/kotlin/com/kavi/pbc/web/event/ui/list/EventList.kt
+++ ui-event/src/commonMain/kotlin/com/kavi/pbc/web/event/ui/list/EventList.kt
@@ -11,9 +11,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.HorizontalDivider
@@ -26,10 +29,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import com.kavi.pbc.web.common.ui.theme.PBCFontFamily
 import com.kavi.pbc.web.event.ui.common.EventItem
 import com.kavi.pbc.web.event.ui.common.EventListItem
 import com.kavi.pbc.web.common.ui.util.ScreenType
@@ -39,14 +44,13 @@ import org.jetbrains.compose.resources.stringResource
 import pbcwebapp.ui_event.generated.resources.Res
 import pbcwebapp.ui_event.generated.resources.event_label_past
 import pbcwebapp.ui_event.generated.resources.event_label_upcoming
+import pbcwebapp.ui_event.generated.resources.event_phrase_past
+import pbcwebapp.ui_event.generated.resources.event_phrase_upcoming
 
 @Composable
 fun EventListUI(navController: NavController) {
 
     val viewModel: EventListViewModel = viewModel { EventListViewModel() }
-
-    val upcomingEventList by viewModel.upcomingEventList.collectAsState()
-    val pastEventList by viewModel.pastEventList.collectAsState()
 
     BoxWithConstraints(
         modifier = Modifier.fillMaxSize(),
@@ -54,18 +58,128 @@ fun EventListUI(navController: NavController) {
     ) {
         val maxWidth = this.maxWidth
 
-        LaunchedEffect(Unit) {
-            viewModel.fetchUpcomingEvents()
-            when (UIUtil.screenType(maxWidth)) {
-                ScreenType.PHONE -> viewModel.fetchPastEventsWithLimit()
-                ScreenType.TABLET, ScreenType.COMPUTER -> viewModel.fetchPastEvents()
+        when (UIUtil.screenType(maxWidth)) {
+            ScreenType.PHONE -> EventListPhoneUI(
+                viewModel = viewModel,
+                navController = navController,
+                maxWidth = maxWidth
+            )
+            ScreenType.TABLET, ScreenType.COMPUTER -> EventListWebUI(
+                viewModel = viewModel,
+                navController = navController,
+                maxWidth = maxWidth
+            )
+        }
+    }
+}
+
+@Composable
+private fun EventListPhoneUI(viewModel: EventListViewModel, navController: NavController, maxWidth: Dp) {
+    val upcomingEventList by viewModel.upcomingEventList.collectAsState()
+    val pastEventList by viewModel.pastEventList.collectAsState()
+
+    LaunchedEffect(Unit) {
+        viewModel.fetchUpcomingEvents()
+        viewModel.fetchPastEventsWithLimit()
+    }
+
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
+    ) {
+        Text(
+            modifier = Modifier
+                .padding(start = 8.dp, top = 12.dp, bottom = 12.dp),
+            text = stringResource(Res.string.event_label_upcoming),
+            textAlign = TextAlign.Justify,
+            fontWeight = FontWeight.Bold,
+            fontFamily = PBCFontFamily,
+            fontSize = 22.sp
+        )
+
+        Row (
+            modifier = Modifier
+                .fillMaxWidth()
+                .horizontalScroll(rememberScrollState()),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            upcomingEventList.forEach { event ->
+                EventItem(screenMaxWidth = maxWidth, event = event, onClick = {
+                    /**
+                     * Alternative way to do the same navigation as a path
+                     * navController.navigate("event/event-selected/${event.id}")
+                     */
+                    navController.navigate(EventPath.EventDetails(eventId = event.id!!))
+                })
             }
         }
 
-        Column (
+        Spacer(modifier = Modifier.height(10.dp))
+
+        HorizontalDivider(
+            modifier = Modifier.padding(2.dp),
+            thickness = 2.dp
+        )
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        Text(
             modifier = Modifier
-                .fillMaxSize()
-                .verticalScroll(rememberScrollState())
+                .padding(start = 8.dp, top = 12.dp, bottom = 12.dp),
+            text = stringResource(Res.string.event_label_past),
+            textAlign = TextAlign.Justify,
+            fontWeight = FontWeight.Bold,
+            fontFamily = PBCFontFamily,
+            fontSize = 22.sp
+        )
+
+        Row (
+            modifier = Modifier.height(600.dp)
+        ) {
+            val columCount = when (UIUtil.screenType(maxWidth)) {
+                ScreenType.PHONE -> 1
+                ScreenType.TABLET -> 2
+                ScreenType.COMPUTER -> 3
+            }
+
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(columCount),
+            ) {
+                items(pastEventList) { event ->
+                    EventListItem(
+                        event = event,
+                        modifier = Modifier.clickable {
+                            /**
+                             * Alternative way to do the same navigation as a path
+                             * navController.navigate("event/event-selected/${event.id}")
+                             */
+                            navController.navigate(EventPath.EventDetails(eventId = event.id!!))
+                        }
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun EventListWebUI(viewModel: EventListViewModel, navController: NavController, maxWidth: Dp) {
+    val upcomingEventList by viewModel.upcomingEventList.collectAsState()
+    val pastEventList by viewModel.pastEventList.collectAsState()
+
+    LaunchedEffect(Unit) {
+        viewModel.fetchUpcomingEvents()
+        viewModel.fetchPastEvents()
+    }
+
+    Row (
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Column (
+            modifier = Modifier.weight(.7f)
         ) {
             Text(
                 modifier = Modifier
@@ -73,17 +187,33 @@ fun EventListUI(navController: NavController) {
                 text = stringResource(Res.string.event_label_upcoming),
                 textAlign = TextAlign.Justify,
                 fontWeight = FontWeight.Bold,
+                fontFamily = PBCFontFamily,
+                fontSize = 40.sp
+            )
+
+            Text(
+                modifier = Modifier
+                    .padding(start = 8.dp, top = 12.dp, bottom = 12.dp),
+                text = stringResource(Res.string.event_phrase_upcoming),
+                textAlign = TextAlign.Start,
+                fontWeight = FontWeight.Light,
+                fontFamily = PBCFontFamily,
                 fontSize = 22.sp
             )
 
-            Row (
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .horizontalScroll(rememberScrollState()),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.Center
+            val columCount = when (UIUtil.screenType(maxWidth)) {
+                ScreenType.PHONE -> 0 // This will not consider in this case
+                ScreenType.TABLET -> 2
+                ScreenType.COMPUTER -> 3
+            }
+
+            LazyVerticalGrid(
+                modifier = Modifier.padding(top = 8.dp),
+                columns = GridCells.Fixed(columCount),
+                horizontalArrangement = Arrangement.Center,
+                verticalArrangement = Arrangement.Center
             ) {
-                upcomingEventList.forEach { event ->
+                items(upcomingEventList) { event ->
                     EventItem(screenMaxWidth = maxWidth, event = event, onClick = {
                         /**
                          * Alternative way to do the same navigation as a path
@@ -93,49 +223,47 @@ fun EventListUI(navController: NavController) {
                     })
                 }
             }
+        }
 
-            Spacer(modifier = Modifier.height(10.dp))
+        Spacer(modifier = Modifier.width(12.dp))
 
-            HorizontalDivider(
-                modifier = Modifier.padding(2.dp),
-                thickness = 2.dp
-            )
-
-            Spacer(modifier = Modifier.height(10.dp))
-
+        Column (
+            modifier = Modifier.weight(.3f)
+        ) {
             Text(
                 modifier = Modifier
                     .padding(start = 8.dp, top = 12.dp, bottom = 12.dp),
                 text = stringResource(Res.string.event_label_past),
                 textAlign = TextAlign.Justify,
-                fontWeight = FontWeight.Bold,
-                fontSize = 22.sp
+                fontWeight = FontWeight.Normal,
+                fontFamily = PBCFontFamily,
+                fontSize = 28.sp
             )
 
-            Row (
-                modifier = Modifier.height(600.dp)
-            ) {
-                val columCount = when (UIUtil.screenType(maxWidth)) {
-                    ScreenType.PHONE -> 1
-                    ScreenType.TABLET -> 2
-                    ScreenType.COMPUTER -> 3
-                }
+            Text(
+                modifier = Modifier
+                    .padding(start = 8.dp, top = 12.dp, bottom = 12.dp),
+                text = stringResource(Res.string.event_phrase_past),
+                textAlign = TextAlign.Start,
+                fontWeight = FontWeight.Light,
+                fontFamily = PBCFontFamily,
+                fontSize = 18.sp
+            )
 
-                LazyVerticalGrid(
-                    columns = GridCells.Fixed(columCount),
-                ) {
-                    items(pastEventList) { event ->
-                        EventListItem(
-                            event = event,
-                            modifier = Modifier.clickable {
-                                /**
-                                 * Alternative way to do the same navigation as a path
-                                 * navController.navigate("event/event-selected/${event.id}")
-                                 */
-                                navController.navigate(EventPath.EventDetails(eventId = event.id!!))
-                            }
-                        )
-                    }
+            LazyColumn (
+                modifier = Modifier.padding(top = 8.dp),
+            ) {
+                items(items = pastEventList) { event ->
+                    EventListItem(
+                        event = event,
+                        modifier = Modifier.clickable {
+                            /**
+                             * Alternative way to do the same navigation as a path
+                             * navController.navigate("event/event-selected/${event.id}")
+                             */
+                            navController.navigate(EventPath.EventDetails(eventId = event.id!!))
+                        }
+                    )
                 }
             }
         }
