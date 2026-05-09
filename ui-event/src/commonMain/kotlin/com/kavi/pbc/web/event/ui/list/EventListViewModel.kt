14ca4641f7f996a6655b216f292cbc24ddce5e47
Kavimal Wijewardana <kavimalw@gmail.com>
2026-05-08 21:56:48 -0400

    Add simple loading for event dashboard screen.

--- ui-event/src/commonMain/kotlin/com/kavi/pbc/web/event/ui/list/EventListViewModel.kt
+++ ui-event/src/commonMain/kotlin/com/kavi/pbc/web/event/ui/list/EventListViewModel.kt
@@ -3,6 +3,7 @@ package com.kavi.pbc.web.event.ui.list
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.kavi.pbc.web.data.event.Event
+import com.kavi.pbc.web.event.data.model.EventListUiState
 import com.kavi.pbc.web.event.data.repository.remote.EventRemoteRepository
 import com.kavi.pbc.web.network.model.ResultWrapper
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -16,19 +17,30 @@ class EventListViewModel: ViewModel() {
     private val _upcomingEventList = MutableStateFlow<List<Event>>(mutableListOf())
     val upcomingEventList: StateFlow<List<Event>> = _upcomingEventList
 
+    private val _upcomingEventListUiState = MutableStateFlow(EventListUiState.NONE)
+    val upcomingEventListUiState: StateFlow<EventListUiState> = _upcomingEventListUiState
+
     private val _pastEventList = MutableStateFlow<List<Event>>(mutableListOf())
     val pastEventList: StateFlow<List<Event>> = _pastEventList
+
+    private val _pastEventListUiState = MutableStateFlow(EventListUiState.NONE)
+    val pastEventListUiState: StateFlow<EventListUiState> = _pastEventListUiState
 
     private val _recurringEventList = MutableStateFlow<List<Event>>(mutableListOf())
     val recurringEventList: StateFlow<List<Event>> = _recurringEventList
 
+    private val _recurringEventListUiState = MutableStateFlow(EventListUiState.NONE)
+    val recurringEventListUiState: StateFlow<EventListUiState> = _recurringEventListUiState
+
     fun fetchUpcomingEvents() {
+        _upcomingEventListUiState.value = EventListUiState.PENDING
         viewModelScope.launch {
             when(val response = eventRemoteRepository.getUpcomingEvents()) {
-                is ResultWrapper.NetworkError -> {}
-                is ResultWrapper.HttpError -> {}
-                is ResultWrapper.UnAuthError -> {}
+                is ResultWrapper.NetworkError, is ResultWrapper.HttpError, is ResultWrapper.UnAuthError -> {
+                    _upcomingEventListUiState.value = EventListUiState.FAILURE
+                }
                 is ResultWrapper.Success -> {
+                    _upcomingEventListUiState.value = EventListUiState.SUCCESS
                     response.value.body?.let {
                         _upcomingEventList.value = it
                     }
@@ -39,11 +51,13 @@ class EventListViewModel: ViewModel() {
 
     fun fetchRecurringEvents() {
         viewModelScope.launch {
+            _recurringEventListUiState.value = EventListUiState.PENDING
             when(val response = eventRemoteRepository.getRecurringEvents()) {
-                is ResultWrapper.NetworkError -> {}
-                is ResultWrapper.HttpError -> {}
-                is ResultWrapper.UnAuthError -> {}
+                is ResultWrapper.NetworkError, is ResultWrapper.HttpError, is ResultWrapper.UnAuthError -> {
+                    _recurringEventListUiState.value = EventListUiState.FAILURE
+                }
                 is ResultWrapper.Success -> {
+                    _recurringEventListUiState.value = EventListUiState.SUCCESS
                     response.value.body?.let {
                         _recurringEventList.value = it
                     }
@@ -54,11 +68,13 @@ class EventListViewModel: ViewModel() {
 
     fun fetchPastEvents() {
         viewModelScope.launch {
+            _pastEventListUiState.value = EventListUiState.PENDING
             when(val response = eventRemoteRepository.getPastEvents()) {
-                is ResultWrapper.NetworkError -> {}
-                is ResultWrapper.HttpError -> {}
-                is ResultWrapper.UnAuthError -> {}
+                is ResultWrapper.NetworkError, is ResultWrapper.HttpError, is ResultWrapper.UnAuthError -> {
+                    _pastEventListUiState.value = EventListUiState.FAILURE
+                }
                 is ResultWrapper.Success -> {
+                    _pastEventListUiState.value = EventListUiState.SUCCESS
                     response.value.body?.let {
                         _pastEventList.value = it
                     }
